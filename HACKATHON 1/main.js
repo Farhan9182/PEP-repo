63a1dbba5699e1dabac2f01ca3a39905fcf54d0e
Farhan9182 <farhaankhaan930@gmail.com>
2021-04-19 02:35:20 +0530

    till payment

--- HACKATHON 1/main.js
+++ HACKATHON 1/main.js
@@ -11,7 +11,7 @@ let passenger = require("./passengerList.js");
         console.log(passenger[1].Name);
         console.log(passenger[2].Name);
         
-        let mob = 9334814816;
+        let mob = "9334814816";
         let userClass = 4;
         let userCategory = 0;
         let userdate = "22/04/2021";
@@ -50,7 +50,7 @@ let passenger = require("./passengerList.js");
             
             await newPage.type("input[id='nlpAnswer']",ans,{delay: 200});
             await newPage.click("span button[type='submit']");
-            await newPage.waitForTimeout(5000);
+            await newPage.waitForTimeout(7000);
 
             let exists = await newPage.$('div[class="ng-tns-c45-13 ui-dialog-content ui-widget-content"]');
             if(exists === null){
@@ -62,7 +62,7 @@ let passenger = require("./passengerList.js");
         //Login Done
 
         //Fill station details
-        await newPage.waitForTimeout(5000);
+        
         await newPage.type('input[aria-controls="pr_id_1_list"]',"PNBE\n",{delay:200});
         await newPage.type('input[aria-controls="pr_id_2_list"]',"HWH\n",{delay:200});
         await newPage.click('span[class="ng-tns-c66-11 ui-dropdown-label ui-inputtext ui-corner-all ng-star-inserted"]');
@@ -104,9 +104,10 @@ let passenger = require("./passengerList.js");
             await newPage.waitForTimeout(2000);
             times--;
         }
-        await newPage.waitForNavigation({waitUntil: 'load', timeout: 0});
+        
+        
+        await newPage.waitForSelector('input[placeholder="Passenger Name"]',{waitUntil: 'load', timeout: 0});
         console.log("Reached Here");
-        await newPage.waitForSelector('input[placeholder="Passenger Name"]');
         console.log(nop);
         console.log(objLength);
         while(nop > 0){
@@ -117,26 +118,44 @@ let passenger = require("./passengerList.js");
             nop--;
         }
         await newPage.waitForSelector('input[placeholder="Passenger Name"]');
-        for(let i =0; i< objLength; i++){
-            await newPage.evaluate((passenger,i)=>{
-                let elem = document.querySelectorAll('input[placeholder="Passenger Name"]');
-                elem[i].setAttribute("value",passenger[i + 1].Name);
-            },passenger,i)
+        
+        let name = await newPage.$$('input[placeholder="Passenger Name"]');
+            
+        for(let i=0; i<name.length; i++){
+            await newPage.evaluate((dname,di)=>{
+                dname.setAttribute("name-index",di);
+            },name[i],i)
+        }
+
+        let age = await newPage.$$('input[placeholder="Age"]');
+            
+        for(let i=0; i<age.length; i++){
+            await newPage.evaluate((dname,di)=>{
+                dname.setAttribute("age-index",di);
+            },age[i],i)
         }
         
-        for(let i =0; i< objLength; i++){
-            await newPage.evaluate((passenger,i)=>{
-                let elem = document.querySelectorAll('input[placeholder="Age"]');
-                elem[i].setAttribute("value",passenger[i + 1].Age);
-            },passenger,i)
+        let gender = await newPage.$$('select[formcontrolname="passengerGender"]');
+            
+        for(let i=0; i<gender.length; i++){
+            await newPage.evaluate((dname,di)=>{
+                dname.setAttribute("gender-index",di);
+            },gender[i],i)
         }
 
         for(let i =0; i< objLength; i++){
-            await newPage.evaluate((passenger,i)=>{
-                let elem = document.querySelectorAll('select[formcontrolname="passengerGender"]');
-                elem[i].setAttribute("value",passenger[i + 1].Gender);
-            },passenger,i)
+            // input[name-index="0"]
+            await newPage.type("input[name-index='"+i+"']",passenger[i + 1].Name);
+            await newPage.type("input[age-index='"+i+"']",passenger[i + 1].Age);
+            await newPage.type("select[gender-index='"+i+"']",passenger[i + 1].Gender+"/n");
         }
+
+        // for(let i =0; i< objLength; i++){
+        //     await newPage.evaluate((passenger,i)=>{
+        //         let elem = document.querySelectorAll('select[formcontrolname="passengerGender"]');
+        //         elem[i].setAttribute("value",passenger[i + 1].Gender);
+        //     },passenger,i)
+        // }
 
         // const name = await newPage.$$('input[placeholder="Passenger Name"]');
         // const age = await newPage.$$('input[placeholder="Age"]');
@@ -171,6 +190,32 @@ let passenger = require("./passengerList.js");
         // 
         await newPage.click("button[type='submit']");
 
+        do{
+            await newPage.waitForSelector("img[border='0']",{waitUntil: 'load', timeout: 0});
+            let url = await newPage.evaluate(()=>{
+                let image = document.querySelectorAll("img[border='0']");
+                var add = image[1].src;
+                return add;
+            })
+                //Captcha solving function
+
+            let value = await Tesseract.recognize(url,'eng',
+                { 
+                    logger: m => console.log(m) 
+                }
+                )
+            let ans = value.data.text.split(":")[1];
+            ans = ans.trim();    
+            
+            await newPage.type("input[id='nlpAnswer']",ans,{delay: 200});
+            await newPage.click("button[type='submit']");
+            await newPage.waitForTimeout(7000);
+
+            let exists = await newPage.$('#nlpCaptchaOuterContainer');
+            if(exists === null){
+                break;
+            }
+        }while (true);
         // for time: 'span[class="time"]'
         // for train number: div[class="col-sm-5 col-xs-11 train-heading"
        
@@ -180,3 +225,16 @@ let passenger = require("./passengerList.js");
     }
 
 })();
+
+//getting all the input elements array
+/*
+let name = await newPage.evaluate(()=>{
+    let elem = document.querySelectorAll('input[placeholder="Name"]');
+    return elem;
+})
+
+for(let i=0; i<name.length; i++){
+    //using name[i] as a selector
+    await newPage.type(name[i],value[i]);
+}
+*/
