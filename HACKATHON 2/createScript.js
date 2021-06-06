cb20530d54dc3bdb3dd6f0a7075b6ffb1430d0dd
Farhan9182 <farhaankhaan930@gmail.com>
2021-06-06 19:14:39 +0530

    backend for creating form

--- HACKATHON 2/createScript.js
+++ HACKATHON 2/createScript.js
@@ -1,3 +1,7 @@
+const idx = 1;
+let db = null;
+
+
 
 let addbtn = document.getElementById('create');
 addbtn.addEventListener('click',function(){
@@ -5,7 +9,7 @@ addbtn.addEventListener('click',function(){
     sectionDiv.classList.add('section');
     document.querySelector('.outer').appendChild(sectionDiv);
     let select = document.createElement('select');
-    select.setAttribute('name','select[]');
+    select.setAttribute('name','select');
     select.setAttribute('required','required');
     select.classList.add('form__text');
     let option1 = document.createElement('option');
@@ -25,7 +29,7 @@ addbtn.addEventListener('click',function(){
     let input = document.createElement('input');
     input.classList.add('form__text');
     input.setAttribute('type','text');
-    input.setAttribute('name','input[]');
+    input.setAttribute('name','input');
     input.setAttribute('required','required');
 
     sectionDiv.appendChild(input);
@@ -39,3 +43,57 @@ addbtn.addEventListener('click',function(){
         this.parentElement.remove();
     }})
 })
+
+let submitbtn = document.getElementById('submit');
+submitbtn.addEventListener('click', function(){
+ 
+    createDB();
+    setTimeout(function(){
+        let nameC = document.getElementById('creator_name').value;
+        let emailC = document.getElementById('creator_email').value;
+        let valueArr = document.querySelectorAll('input[name="input"]');
+        let optionArr = document.querySelectorAll('select[name="select"]');
+        for(let i=0; i<valueArr.length; i++){
+            const tupple = {
+                name: nameC,
+                email: emailC,
+                idx: i+1,
+                option: optionArr[i].value,
+                label: valueArr[i].value
+            }
+
+            const tx = db.transaction("my_Form", "readwrite")
+            tx.onerror = e => alert( ` Error! ${e.target.error}  `)
+            const pNotes = tx.objectStore("my_Form")
+            pNotes.add(tupple);
+        }
+       }, 3000);
+    //    window.location.reload();
+})
+
+
+function createDB () {
+
+    const dbName = "FormDatabase";
+    const dbVersion = "1";
+
+    const request = indexedDB.open(dbName,dbVersion)
+
+        //on upgrade needed
+        request.onupgradeneeded = e => {
+            db = e.target.result
+            db.createObjectStore("my_Form", {keyPath: "idx"})
+
+        }
+        //on success 
+        request.onsuccess = e => {
+            db = e.target.result
+        }
+        //on error
+        request.onerror = e => {
+            alert(`error: ${e.target.error} was found `)
+             
+        }
+
+
+}
